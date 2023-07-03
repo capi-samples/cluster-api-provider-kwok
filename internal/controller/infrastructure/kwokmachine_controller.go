02cf21d3b0f5eee1448b0e078b55177837a45888
vadasambar <surajrbanakar@gmail.com>
2023-07-03 12:40:08 +0530

    feat: start implementing kwok machine controller

--- internal/controller/infrastructure/kwokmachine_controller.go
+++ internal/controller/infrastructure/kwokmachine_controller.go
@@ -19,10 +19,13 @@ package controller
 import (
 	"context"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	infrastructurev1alpha1 "github.com/capi-samples/cluster-api-provider-kwok/api/infrastructure/v1alpha1"
 )
@@ -47,9 +50,32 @@ type KwokMachineReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
 func (r *KwokMachineReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	kwokMachine := &infrastructurev1alpha1.KwokMachine{}
+	err := r.Get(ctx, req.NamespacedName, kwokMachine)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	// Fetch the machine
+	machine, err := util.GetOwnerMachine(ctx, r.Client, kwokMachine.ObjectMeta)
+	if err != nil {
+		logger.Error(err, "Failed to retrieve owner Machine from the API Server")
+
+		return ctrl.Result{}, err
+	}
+
+	if machine == nil {
+		logger.Info("Machine Controller has not yet set OwnerRef")
+
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	logger = logger.WithValues("machine", machine.Name)
 
 	return ctrl.Result{}, nil
 }
